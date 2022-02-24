5e72d31ad229aaf05a8b4e43fc45fda5784eeeae
Noah McCampbell <noahmccamp@aol.com>
2022-02-24 12:08:21 -0500

    Fixed Everything check all scenarios *the deal function messed up purposefully*

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,9 @@ class DoOperations: NSObject, ObservableObject {
     @Published var playerTwoDeck:[card] = []
     @Published var p1OffHand:[card] = []
     @Published var p2OffHand:[card] = []
-    @Published var result:String = ""
+    @Published var result:String = "READY TO START?"
+    @Published var p1BeforeCard:card?
+    @Published var p2BeforeCard:card?
     
     //Card struct main data type
     struct card{
@@ -30,8 +32,9 @@ class DoOperations: NSObject, ObservableObject {
         //print all 52 cards to the print screen and show me the name, suit, rank of all 52 cards.
     }
     func dealDecks(){
-        var deal = false
-        deckOfCards.shuffle()
+        //var deal = false
+        //deckOfCards.shuffle()
+        /*
         for card in deckOfCards{
             if(deal){
                 playerOneDeck.append(card)
@@ -40,66 +43,100 @@ class DoOperations: NSObject, ObservableObject {
             }
             deal.toggle()
         }
+         */
+        for __ in 0...25{
+            playerOneDeck.append(deckOfCards[0])
+            deckOfCards.remove(at: 0)
+        }
+        for __ in 0...25{
+            playerTwoDeck.append(deckOfCards[0])
+            deckOfCards.remove(at: 0)
+        }
+        print(deckOfCards.count)
     }
     func declareWar(){
         //War logic
+        print(playerOneDeck)
         for i in 0...2{
-            p1OffHand.append(playerOneDeck[i])
             p2OffHand.append(playerTwoDeck[i])
-            playerOneDeck.remove(at: i)
-            playerTwoDeck.remove(at: i)
+            p1OffHand.append(playerOneDeck[i])
+            playerTwoDeck.remove(at: 0)
+            playerOneDeck.remove(at: 0)
+            
         }
+        print(p2OffHand)
+        print(p1OffHand)
     }
     func testCard(cardOne: card, cardTwo:card){
+        //SHOW THE CARDS THAT ARE BEING TESTED !!!!!!!!!
+        p1BeforeCard = cardOne
+        p2BeforeCard = cardTwo
+        
         //TESTING CARDS AND HANDLING IF HAVING A WAR
+        
+        //WINS
         if(playerOneDeck.isEmpty){
             result = "PLAYER TWO WON!"
         }
         else if(playerTwoDeck.isEmpty){
             result = "PLAYER ONE WON!"
         }
+        //PLAYER ONE WON
         else if(cardOne.rank > cardTwo.rank){
+            result = "PLAYER ONE WINS"
+            //IF THERE IS A TIE AND THE HANDLIND OF THE OFF HAND ON PLAYER TWO WIN
             if(!p1OffHand.isEmpty){
+                result = "PLAYER ONE WON THE WAR"
                 for i in 0...2{
                     playerOneDeck.append(p1OffHand[i])
                     playerOneDeck.append(p2OffHand[i])
                 }
                 p1OffHand = []
                 p2OffHand = []
-                result = "PLAYER ONE WON THE WAR"
+                
             }else{
+                //NORMAL SITUATION PLAYER ONE WINS
                 playerOneDeck.append(cardTwo)
                 playerTwoDeck.remove(at: 0)
                 playerOneDeck.append(playerOneDeck[0])
                 playerOneDeck.remove(at: 0)
-                result = "PLAYER ONE WINS"
             }
+        //PLAYER 2 WON
         }else if(cardTwo.rank > cardOne.rank){
+            
+            //IF THERE IS A TIE AND THE HANDLIND OF THE OFF HAND ON PLAYER TWO WIN
+            result = "PLAYER TWO WINS"
             if(!p1OffHand.isEmpty){
+                result = "PLAYER TWO WON THE WAR"
                 for i in 0...2{
                     playerTwoDeck.append(p1OffHand[i])
                     playerTwoDeck.append(p2OffHand[i])
                 }
                 p1OffHand = []
                 p2OffHand = []
-                result = "PLAYER TWO WON THE WAR"
+                
             }else{
+                //NORMAL SITUATION PLAYER TWO WINS
                 playerTwoDeck.append(cardOne)
                 playerOneDeck.remove(at: 0)
                 playerTwoDeck.append(playerOneDeck[0])
                 playerTwoDeck.remove(at: 0)
-                result = "PLAYER TWO WINS"
             }
         }else{
+            //TIES
+            //INFINITE TIE LOOP
             if (playerOneDeck.count < 4 && playerTwoDeck.count < 4){
                 result = "THE ULTIMATE DRAW"
             }
-            if(playerOneDeck.count < 4){
+            //PLAYER ONE RAN OUT OF CARDS
+            else if(playerOneDeck.count < 4){
                 result = "PLAYER ONE LOSES NOT ENOUGH CARDS"
             }
+            //PLAYER TWO RAN OUT OF CARDS
             else if(playerOneDeck.count < 4){
                 result = "PLAYER ONE LOSES NOT ENOUGH CARDS"
             } else {
+                //IF THERE IS A WAR HANDLING
                 declareWar()
                 result = "WAR"
             }
@@ -117,6 +154,8 @@ class DoOperations: NSObject, ObservableObject {
         
        createDeck()
         dealDecks()
+        print(playerTwoDeck)
+        print(playerOneDeck)
     }
 }
 
@@ -126,9 +165,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text(op.result)
-                .font(.title)
-             
+            
             // Player Card Button
             Button{
                 isTesting.toggle()
@@ -152,17 +189,28 @@ struct ContentView: View {
                         }
                         //PLAYING VIEW
                         else if(!op.playerTwoDeck.isEmpty){
-                            op.playerTwoDeck[0].cardImage
+                            op.p2BeforeCard?.cardImage
                                 .resizable()
                                 .scaledToFit()
                         }else{
                             Text("YOU LOST :(")
                                 .font(.largeTitle)
                         }
+                        //OFF HAND VISUALS
                         if(!op.p2OffHand.isEmpty){
-                            op.p2OffHand[0].cardImage
-                                .resizable()
-                                .scaledToFit()
+                             ZStack{
+                                 Image("back")
+                                     .resizable()
+                                     .scaledToFit()
+                                 Image("back")
+                                     .resizable()
+                                     .scaledToFit()
+                                     .rotationEffect(.degrees(5.0))
+                                 Image("back")
+                                     .resizable()
+                                     .scaledToFit()
+                                     .rotationEffect(.degrees(10.0))
+                            }
                         }
                     }.rotationEffect(.degrees(180.0))
                     // PLAYER ONE SIDE
@@ -180,26 +228,36 @@ struct ContentView: View {
                         }
                         //PLAYING VIEW
                         else if(!op.playerOneDeck.isEmpty){
-                            op.playerOneDeck[0].cardImage
+                            op.p1BeforeCard?.cardImage
                                 .resizable()
                                 .scaledToFit()
                         }else{
                             Text("YOU LOST :(")
                                 .font(.largeTitle)
                         }
-                        if(!op.p1OffHand.isEmpty){
-                            op.p1OffHand[0].cardImage
-                                .resizable()
-                                .scaledToFit()
-                        }
-                    }
+                        //OFF HAND VISUALS
+                       if(!op.p1OffHand.isEmpty){
+                            ZStack{
+                                Image("back")
+                                    .resizable()
+                                    .scaledToFit()
+                                Image("back")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .rotationEffect(.degrees(5.0))
+                                Image("back")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .rotationEffect(.degrees(10.0))
+                                }
+                       }
                 }
-            
+                    
             }
             //Description of card shown
-            Text("Player 1 Deck Count: \(op.playerOneDeck.count) Player 2 Deck Count: \(op.playerTwoDeck.count)").font(.subheadline)
+            //Text("Player 1 Deck Count: \(op.playerOneDeck.count) Player 2 Deck Count: \(op.playerTwoDeck.count)").font(.subheadline)
             
-            //Buttons to shuffle, re-arrange, etc. (DEV MENU)
+            //Buttons to shuffle, re-arrange, etc. (OLD DEBUG MENU)
             /*HStack{
                 Button{
                     op.deckOfCards.shuffle()
@@ -223,5 +281,8 @@ struct ContentView: View {
             }
             */
         }
+            Text(op.result)
+                .font(.title)
     }
+}
 }
